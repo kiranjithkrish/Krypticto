c6d3360984c3798b6cc85c57945e8998d8cc327c
Code0Light1 <101674210+Code0Light1@users.noreply.github.com>
2024-07-09 08:28:34 +0100

    Initial Commit

---
+++ Krypticto/KryptictoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KryptictoApp.swift
+//  Krypticto
+//
+//  Created by kiranjith on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct KryptictoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
